d91560002ba54d809fc75673ca3a3faad3a4ffd1
morantaf <fall_moranta@live.fr>
2020-05-28 10:19:11 +0200

    Creating the User table

---
+++ UserModel.js
@@ -0,0 +1,33 @@
+const db = require("./db");
+const Sequelize = require("sequelize");
+
+const User = db.define(
+  "users",
+  {
+    username: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    firstName: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
+
+module.exports = User;
