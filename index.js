b39befcb8e96cadccef843b3399ff482c2f731fe
morantaf <fall_moranta@live.fr>
2020-05-28 10:15:21 +0200

    Creating the GraphQL server

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+
+const app = express();
+const port = 4000;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.applyMiddleware({ app });
+
+app.listen(port, () =>
+  console.log(`Server running on port ${port}` + server.graphqlPath)
+);
