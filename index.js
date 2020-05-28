93fe234995740664a88beeaca379fa6646e47ec4
morantaf <fall_moranta@live.fr>
2020-05-28 10:18:16 +0200

    Initializing the database using Sequelize

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { ApolloServer } = require("apollo-server-express");
 
 const app = express();
 const port = 4000;
+const db = require("./db");
 
 const server = new ApolloServer({
   typeDefs,
