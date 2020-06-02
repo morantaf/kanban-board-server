f611cd89832ae57ecf96df4c416a28ccddfbef65
nikolasalim <nikolasalim@gmail.com>
2020-06-02 14:29:46 +0200

    Test push

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
+//
 
 const app = express();
 const port = 4000;
